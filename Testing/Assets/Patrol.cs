5c938ae24a507f5b3bda1c1dd9368cb09f4e3bea
Hunter Lintz <hunterlintz126@gmail.com>
2020-08-30 11:37:21 -0400

    Added a Enemy Patrol script that does not work lmfao.

---
+++ Testing/Assets/Patrol.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+	public float speed;
+    public float distance;
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+	public LayerMask whatIsGround;
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate(Vector2.right * speed * Time.deltaTime);
+    	RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector3.down, distance, whatIsGround);
+        Debug.DrawRay(groundDetection.position, Vector3.down, Color.green);
+		if(groundInfo.collider == false)
+        {
+            if(movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+				Debug.Log("poop");
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+
+    }
+}
---
+++ Testing/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+	public float speed;
+    public float distance;
+    public bool movingRight = true;
+	public bool isTouching;
+
+    public Transform groundDetection;
+	public LayerMask whatIsGround;
+
+    // Update is called once per frame
+    void fixedUpdate()
+    {
+		transform.Translate(Vector3.right * speed * Time.deltaTime);
+    	RaycastHit hit;
+		if(Physics.Raycast(groundDetection.position, Vector3.down,out hit, distance) == true){
+			isTouching = true;
+            if(movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+		}
+        Debug.DrawRay(groundDetection.position, Vector3.down, Color.green);
+
+    }
+}
