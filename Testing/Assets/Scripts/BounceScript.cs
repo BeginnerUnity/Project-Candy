62f54602001cc8017b1782a08ac68901538b0f5a
Symby <t.stevenson.91nl@gmail.com>
2020-09-04 20:06:04 +0200

    hardcoded movement to powerups + may want to add vexel art but think it looks fine

---
+++ Testing/Assets/Scripts/BounceScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceScript : MonoBehaviour
+{
+    public bool active = true;
+    public float range = 1;
+    public float speed = 1;
+
+    private Vector3 startPosition;
+    private bool movingDown = true;
+    private float displacement = 0f;
+
+    
+    void Awake()
+    {
+        startPosition = this.transform.localPosition;
+    }
+
+    void Update()
+    {
+        if(active)
+        {
+            if(movingDown)
+            {
+                if(displacement <= range)
+                {
+                    this.transform.Translate(Vector3.down * speed * Time.deltaTime);
+                    displacement = displacement + (speed * Time.deltaTime);
+                }
+                else
+                {
+                    movingDown = false;
+                }
+            }
+            else
+            {
+                if(displacement >= 0)
+                {
+                    this.transform.Translate(Vector3.up * speed * Time.deltaTime);
+                    displacement = displacement - (speed * Time.deltaTime);
+                }
+                else
+                {
+                    movingDown = true;
+                }
+            }
+        }
+    }
+}
